a9294ed23d0380664f97f892f3e9c2697473f208
kota <fujiwara.kota@gmail.com>
2012-02-20 23:34:38 +0900

    持ち駒(なし)表示を追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,12 @@
 # -*- coding: utf-8 -*-
 module ApplicationHelper
+
+  def h_answer(problem)
+    if problem.hand
+      image_tag(problem.answer_image)
+    else
+      "(なし)"
+    end
+  end
+
 end
